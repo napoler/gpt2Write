5ef5a1de0977adcd49f63b28a679f60e4eae9cf7
Terry chan <napoler2008@gmail.com>
2019-09-10 17:37:05 +0800

    加入写作编辑器
    删除
    Signed-off-by: Terry chan <napoler2008@gmail.com>

---
+++ app.py
@@ -0,0 +1,52 @@
+
+from flask import Flask, render_template, request, json, Response, jsonify,escape
+from generate import ai
+app = Flask(__name__)
+
+def get_post_data():
+    """
+    从请求中获取参数
+    :return:
+    """
+    data = {}
+    if request.content_type.startswith('application/json'):
+        data = request.get_data()
+        data = json.loads(data)
+    else:
+        for key, value in request.form.items():
+            if key.endswith('[]'):
+                data[key[:-2]] = request.form.getlist(key)
+            else:
+                data[key] = value
+    return data
+
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+@app.route('/edit')
+def edit():
+    return render_template("edit.html")
+# 获取预测结果
+@app.route("/json/predict",methods=['GET', 'POST'])
+def json_predict():
+    # #句子
+
+    data= get_post_data()
+    print('data',data)
+    # paragraph = request.args.get('text')
+    # previous_line=request.args.get('sentence')
+    text = data['text']
+    text_array={'original':text,
+    'items':ai(text)
+    }
+    
+ 
+
+    return jsonify(text_array)
+
+
+if __name__ == "__main__":
+    app.run()
--- generate.py
+++ generate.py
@@ -127,7 +127,7 @@ def main():
                         help='模型参数')
     parser.add_argument('--tokenizer_path', default='cache/vocab_small.txt', type=str, required=False, help='词表路径')
     parser.add_argument('--model_path', default='model/final_model', type=str, required=False, help='模型路径')
-    parser.add_argument('--prefix', default='萧炎', type=str, required=False, help='生成文章的开头')
+    parser.add_argument('--prefix', default='哈士奇', type=str, required=False, help='生成文章的开头')
     parser.add_argument('--no_wordpiece', action='store_true', help='不做word piece切词')
     parser.add_argument('--segment', action='store_true', help='中文以词为单位')
     parser.add_argument('--fast_pattern', action='store_true', help='采用更加快的方式生成文本')
@@ -206,6 +206,114 @@ def main():
             if args.save_samples:
                 samples_file.close()
             break
+
+
+
+
+def ai(text,length=20):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', default='0,1,2,3', type=str, required=False, help='生成设备')
+    parser.add_argument('--length', default=length, type=int, required=False, help='生成长度')
+    parser.add_argument('--batch_size', default=1, type=int, required=False, help='生成的batch size')
+    parser.add_argument('--nsamples', default=5, type=int, required=False, help='生成几个样本')
+    parser.add_argument('--temperature', default=1, type=float, required=False, help='生成温度')
+    parser.add_argument('--topk', default=8, type=int, required=False, help='最高几选一')
+    parser.add_argument('--topp', default=0, type=float, required=False, help='最高积累概率')
+    parser.add_argument('--model_config', default='config/model_config_small.json', type=str, required=False,
+                        help='模型参数')
+    parser.add_argument('--tokenizer_path', default='cache/vocab_small.txt', type=str, required=False, help='词表路径')
+    parser.add_argument('--model_path', default='model/final_model', type=str, required=False, help='模型路径')
+    parser.add_argument('--prefix', default=text, type=str, required=False, help='生成文章的开头')
+    parser.add_argument('--no_wordpiece', action='store_true', help='不做word piece切词')
+    parser.add_argument('--segment', action='store_true', help='中文以词为单位')
+    parser.add_argument('--fast_pattern', action='store_true', help='采用更加快的方式生成文本')
+    parser.add_argument('--save_samples', action='store_true', help='保存产生的样本')
+    parser.add_argument('--save_samples_path', default='.', type=str, required=False, help="保存样本的路径")
+
+    args = parser.parse_args()
+    print('args:\n' + args.__repr__())
+
+    if args.no_wordpiece:
+        from tokenizations import tokenization_bert_without_wordpiece as tokenization_bert
+    elif args.segment:
+        from tokenizations import tokenization_bert_word_level as tokenization_bert
+    else:
+        from tokenizations import tokenization_bert
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.device  # 此处设置程序使用哪些显卡
+    length = args.length
+    batch_size = args.batch_size
+    nsamples = args.nsamples
+    temperature = args.temperature
+    topk = args.topk
+    topp = args.topp
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    tokenizer = tokenization_bert.BertTokenizer(vocab_file=args.tokenizer_path)
+    model = GPT2LMHeadModel.from_pretrained(args.model_path)
+    model.to(device)
+    model.eval()
+
+    if length == -1:
+        length = model.config.n_ctx - len(args.prefix)
+    elif length > model.config.n_ctx - len(args.prefix):
+        raise ValueError("Can't get samples longer than window size: %s" % model.config.n_ctx)
+    if args.save_samples:
+        if not os.path.exists(args.save_samples_path):
+            os.makedirs(args.save_samples_path)
+        samples_file = open(args.save_samples_path + '/samples.txt', 'w', encoding='utf8')
+    while True:
+        raw_text = args.prefix
+        context_tokens = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(raw_text))
+        generated = 0
+        all_text=[]
+        for _ in range(nsamples // batch_size):
+            out = generate(
+                model=model,
+                context=context_tokens,
+                length=length,
+                is_fast_pattern=args.fast_pattern,
+                temperature=temperature, top_k=topk, top_p=topp, device=device
+            )
+            for i in range(batch_size):
+                generated += 1
+                text = tokenizer.convert_ids_to_tokens(out)
+                for i, item in enumerate(text[:-1]):  # 确保英文前后有空格
+                    if is_word(item) and is_word(text[i + 1]):
+                        text[i] = item + ' '
+                for i, item in enumerate(text):
+                    if item == '[MASK]':
+                        text[i] = ''
+                    if item == '[CLS]' or item == '[SEP]':
+                        text[i] = '\n'
+                info = "=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40 + "\n"
+                print(info)
+                text = ''.join(text).replace('##', '').strip()
+                print(text)
+                all_text.append(text)
+                if args.save_samples:
+                    samples_file.write(info)
+                    samples_file.write(text)
+                    samples_file.write('\n')
+                    samples_file.write('=' * 90)
+                    samples_file.write('\n' * 2)
+        print("=" * 80)
+        return all_text
+        if generated == nsamples:
+            # close file when finish writing.
+            if args.save_samples:
+                samples_file.close()
+            break
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
